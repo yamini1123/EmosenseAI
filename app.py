175fa96554aafeef2acaa307ac26ef6437fee9ca
yamini1123 <169121558+yamini1123@users.noreply.github.com>
2026-03-29 23:01:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from sklearn.naive_bayes import MultinomialNB
 # Training Data
 # -----------------------
 texts = [
-    "I feel amazing today", "I am very happy", "best day ever",
-    "I am feeling great", "I am so happy",
+    # "I feel amazing today", "I am very happy", "best day ever",
+    # "I am feeling great", "I am so happy",
 
     "I feel so low", "I am really upset", "nothing is going right",
     "I feel tired and sad", "I want to cry",
@@ -20,15 +20,31 @@ texts = [
     "I am nervous", "something feels wrong",
 
     "I completed my work", "today was normal",
-    "I am going outside", "nothing special today", "just another day"
+    "I am going outside", "nothing special today", "just another day",
+    
+     "I feel underconfident because I have not achieved anything great so far","","","","",
+    "I think people look down at me and also I feel judged","","","","",
+
+    "hello","","","","",
+
+    "I feel like I’m not good enough… I always mess things up.","","","","",
+    "But others seem so confident, I don’t even feel close to them.","","","","",
+    "What if I fail again?","","","","",
 ]
 
 labels = [
-    "happy","happy","happy","happy","happy",
+    # "happy","happy","happy","happy","happy",
     "sad","sad","sad","sad","sad",
     "angry","angry","angry","angry","angry",
     "fear","fear","fear","fear","fear",
-    "neutral","neutral","neutral","neutral","neutral"
+    "neutral","neutral","neutral","neutral","neutral",
+    "underconfident","underconfident","underconfident","underconfident","underconfident",
+    "judged","judged","judged","judged","judged",
+    "first-text", "first-text", "first-text", "first-text", "first-text",
+    "goodenough","goodenough","goodenough","goodenough","goodenough",
+    "others", "others", "others", "others", "others",
+    "fail","fail","fail","fail","fail",
+
 ]
 
 # -----------------------
@@ -48,12 +64,13 @@ def predict_emotion(text):
 # Responses
 # -----------------------
 responses = {
-    "happy": [
-        "That’s amazing! What made your day?",
-        "Glad to hear that. Tell me more."
-    ],
+
+    # "happy": [
+    #     "That’s amazing! What made your day?",
+    #     "Glad to hear that. Tell me more."
+    # ],
     "sad": [
-        "I’m really sorry you’re feeling this way. I’m here for you.",
+        "I’m really sorry you’re feeling this way. I’m here for you.\n Do you want to share about it more?",
         "That sounds tough. Do you want to talk about it?"
     ],
     "angry": [
@@ -67,7 +84,28 @@ responses = {
     "neutral": [
         "Got it. Tell me more.",
         "Alright. What else is on your mind?"
-    ]
+
+    ],
+    "underconfident": [
+        "It is okay to feel low sometimes, but remember that your current achievements do not define your potential. "
+    ],
+    "judged":[
+        "Yeah! that feeling is very common and can be pretty heavy.When you feel underconfident your mind kind of goes into spotlight mode like everyone is watching and judging you. But there is a small truth here - people are already busy in their life but they might notice your small voice and hesitation so avoid eye contact."
+    ],
+  "first-text":[
+         "Hey! How can I help you today?"],
+
+
+ "goodenough":[
+     "You’re being too hard on yourself—everyone makes mistakes, that doesn’t define you.",
+],
+ "others":[
+  "Confidence grows with time, and you’re already stronger than you think."
+],
+ "fail":[
+        "Then you’ll learn again—and that’s how you get better, step by step"
+ ],
+
 }
 
 # -----------------------
